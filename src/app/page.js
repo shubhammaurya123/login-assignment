7b8aa3974c2e9dcd1c71e6cef8c6e4035d21195b
Shubham Maurya <skmaurya9721@gmail.com>
2024-02-22 22:11:28 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,185 @@
+"use client";
 import Image from "next/image";
+import Layer from "../../public/Layer_1.svg";
+import Hey from "../../public/hey-bro.svg";
+import {useState } from "react"; // Ensure useState is imported from 'react'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GoHome } from "react-icons/go";
+import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
+
+// git  add .
+// git commit -m 'frjgnf'
+// git push
+const LOGIN_STATE = {
+  LOGIN_PAGE: "LOGIN_PAGE",
+  LOADING: "LOADING",
+};
+
 
 export default function Home() {
+  const userCredential = {
+    email: "skmaurya9721@gmail.com",
+    password: "Maurya@123",
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [currentState, setCurrentState] = useState(LOGIN_STATE.LOGIN_PAGE);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const notify = () =>
+    toast(
+      <div className="flex gap-2 items-center">
+        <GoHome /> Email or Password incorrect
+      </div>
+    );
+  const handleOnClickLogin = (e) => {
+    e.preventDefault();
+    setCurrentState(LOGIN_STATE.LOADING);
+    setTimeout(() => {
+      setCurrentState(LOGIN_STATE.LOGIN_PAGE);
+      if (
+        email === userCredential.email &&
+        password === userCredential.password
+      ) {
+        alert("Login Successfully");
+      } else {
+        return notify();
+      }
+    }, 2000);
+  };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen bg-white">
+      <section className="flex flex-1 flex-col items-center justify-center bg-red text-black bg-gray-100">
+        <Image src={Layer} alt="no-icon" width={400} height={400} />
+        <div
+          className="block font-medium text-gray-900 mb-4 mt-12"
+          style={{ fontSize: "20px", color: "#556A89" }}
+        >
+          Labour Link: Secure Farming
         </div>
-      </div>
+        <div
+          className="block text-sm text-gray-400"
+          style={{ fontSize: "14px", color: "#556A89" }}
+        >
+          Manage Labour, Personnel, and More...
+        </div>
+      </section>
+      <section className="flex flex-1 bg-white text-black flex-col p-10 justify-center ">
+        <div className="mb-8">
+          <Image src={Hey} alt="no-icon" width={60} height={60} />
+          <div
+            className="block font-medium text-lg dark:text-black mb-3"
+            style={{ fontSize: "26px" }}
+          >
+            Hello HR!
+          </div>
+          <div className="mb-2 text-sm font-medium text-gray-500 font-weight-500">
+            {currentState === LOGIN_STATE.LOADING
+              ? "Trying to log in ..."
+              : "Please Login to access your account"}
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        {currentState === LOGIN_STATE.LOGIN_PAGE && (
+          <form className="w-full" style={{ maxWidth: "94%" }} >
+            <div className="mb-6">
+              <div className="flex justify-between">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                >
+                  Email<span style={{ color: "#FF0000" }}>*</span>
+                </label>
+                <div
+                  className="text-red-900 text-sm bg-#FF7777"
+                  style={{ color: "#FF0000" }}
+                >
+                  Enter valid email ID
+                </div>
+              </div>
+              <input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john.doe@company.com"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+              >
+                Password<span style={{ color: "#FF0000" }}>*</span>
+              </label>
+              <div className="relative">
+                <input
+                  onChange={handlePasswordChange}
+                  value={password}
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-100 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="•••••••••"
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center px-3"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
+                </button>
+              </div>
+            </div>
+
+            <button
+              onClick={handleOnClickLogin}
+              type="submit"
+              className="text-white  bg-blue-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              style={{ width: "100%" }}
+            >
+              Log in
+            </button>
+          </form>
+        )}
+        {currentState === LOGIN_STATE.LOADING && (
+          <div
+            className="bg-gray-100 rounded-l-lg rounded-r-lg mt-3"
+            style={{ width: "80%" }}
+          >
+            <div
+              className="bg-blue-800  rounded-lg"
+              style={{ height: "10px", width: "33%" }}
+            ></div>
+          </div>
+        )}
+        <ToastContainer
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          toastStyle={{ background: "#FF7777", color: "#fff" }}
+          bodyStyle={{ fontSize: "14px" }}
+          progressStyle={{ background: "red", height: "4px" }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   );
 }
